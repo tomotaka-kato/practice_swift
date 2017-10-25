3911412e1889eb027c51b377143e47b1d83ba04b
tomotaka.kato <tomotaka_kato@CCT1411-002.local>
2017-10-25 13:36:20 +0900

    add MyCamera

---
+++ swift4study/MyCamera/MyCamera/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by tomotaka.kato on 2017/10/25.
+//  Copyright © 2017 tomotaka.kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBAction func lcameraLaunchAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera can be used.")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            present(ipc, animated: true, completion: nil)
+        } else {
+            print("Camera is not available")
+        }
+    }
+    @IBAction func shareAction(_ sender: Any) {
+        if let sharedImage = photoImage.image {
+            let sharedItems = [sharedImage]
+            let controller = UIActivityViewController(activityItems: sharedItems, applicationActivities: nil)
+            controller.popoverPresentationController?.sourceView = view
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        photoImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
+}
+
